e827f791f4ec9eaef2c84dae0ca717387ec73c7f
Asher Snavely <Asher_Snavely@baylor.edu>
2018-10-20 13:28:02 -0700

    Implemented a lot more to MessageTableController including sorters and filters

--- BearRides/src/main/java/controller/UserTableController.java
+++ BearRides/src/main/java/controller/UserTableController.java
@@ -5,6 +5,8 @@
 
 package controller;
 
+import java.util.List;
+
 import object.User;
 import table.UserTable;
 
@@ -14,9 +16,9 @@ public class UserTableController {
      * description: attempts to login with the provided info
      * return: User
      * precondition: user is not already logged in, user is registered
-     * postcondition: the user is marked as logged in and 
+     * postcondition: the user is marked as logged in and
      */
-    public static User login(UserTable table) throws Exception {
+    public static User login(UserTable table, String UUID, String Password) throws Exception {
         return null;
     }
     
@@ -24,7 +26,7 @@ public class UserTableController {
         return null;
     }
     
-    public static Boolean remove(UserTable table, String UUID) {
+    public static Boolean remove(UserTable table, User user) {
         return null;
     }
 
@@ -35,4 +37,22 @@ public class UserTableController {
     public static void reverifyAll(UserTable table) {
         // stubbed
     }
+    
+    public static List<String> getPrivateInfo(UserTable table, User user) {
+        return null;
+    }
+    
+    public static List<String> getPublicInfo(UserTable table, User user) {
+        return null;
+    }
+    
+    public static Boolean setInfo(List<String> info) {
+        return null;
+    }
+    
+    
+    // gui -> controller
+    //          |
+    //          v
+    //     <- TableModel
 }
